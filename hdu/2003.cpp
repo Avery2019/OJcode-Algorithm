754a8984fc311290fbb930870d24c22e26b53462
Ding jinge <walkerboudless@gmail.com>
2020-07-15 22:37:35 +0800

    HDU 2003

---
+++ hdu/2003.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double x;
+    while(cin>>x){
+        if(x>=0)
+            printf("%.2lf\n",x);
+        else
+            printf("%.2lf\n",-x);
+    }
+    return 0;
+}
